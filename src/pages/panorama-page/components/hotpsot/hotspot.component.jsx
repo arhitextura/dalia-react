4aff7b4c2bafb354c1c36b249592da7ae90c5da8
dani198912 <bdaniel.mihalcea@gmail.com>
2020-10-28 21:14:43 +0200

    Draggable component allmost finished

--- src/pages/panorama-page/components/hotpsot/hotspot.component.jsx
+++ src/pages/panorama-page/components/hotpsot/hotspot.component.jsx
@@ -2,15 +2,35 @@ import React, { Component } from "react";
 import styles from "./hotspot.module.scss";
 import * as THREE from "three";
 import { ReactComponent as Arrow } from "../../../../icons/arrow_circle_up-24px.svg";
+
+const toScreenPosition = (obj, _camera, _renderer, _scene) => {
+  let { x, y, z } = obj.position;
+  var objVector = new THREE.Vector3(x, y, z);
+
+  var widthHalf = 0.5 * _renderer.getContext().canvas.width;
+  var heightHalf = 0.5 * _renderer.getContext().canvas.height;
+
+  if (_scene.autoUpdate === true) _scene.updateMatrixWorld();
+  if (_camera.parent === null) _camera.updateMatrixWorld();
+  let vector = objVector.project(_camera);
+  const isVisible =
+    obj.visible && vector.z >= -1 && vector.z <= 1 ? "" : "none";
+
+  vector.x = vector.x * widthHalf + widthHalf;
+  vector.y = -vector.y * heightHalf + heightHalf;
+  // console.log(`X: ${vector.x} ==== Y:${vector.y}`);
+  return { posX: vector.x, posY: vector.y, visibility: isVisible };
+};
+
 class Hotspot extends Component {
   constructor() {
     super();
     this.domRef = React.createRef();
-    this.state = {
-      isUserIntercating: false,
-      x: 100,
-      y: 100,
-    };
+    // this.state = {
+    //   isUserIntercating: false,
+    //   x: 100,
+    //   y: 100,
+    // };
     this.temp_geometry = new THREE.SphereBufferGeometry(10, 10, 10);
     this.temp_material = new THREE.MeshBasicMaterial({
       color: 0xffffaf,
@@ -18,20 +38,20 @@ class Hotspot extends Component {
     });
 
     this.ball = new THREE.Mesh(this.temp_geometry, this.temp_material);
+    this.ball.name = "UniqueName";
     this.ball.position.set(50, 0, 100);
+    this.isUserIntercating = false;
+    this.position = {
+      x: 100,
+      y: 100,
+    };
   }
 
   handlePointerDown = (e) => {
     e.stopImmediatePropagation();
     if (e.isPrimary === false) return;
-    this.setState(
-      { isUserIntercating: true }
-    );
-    this.domRef.current.addEventListener(
-      "pointermove",
-      this.handlePointerMove,
-      false
-    );
+    this.isUserIntercating = true;
+    window.addEventListener("pointermove", this.handlePointerMove, false);
     this.domRef.current.addEventListener(
       "pointerup",
       this.handlePointerUp,
@@ -39,64 +59,56 @@ class Hotspot extends Component {
     );
   };
   handlePointerMove = (e) => {
-    e.stopImmediatePropagation();
-    if (this.state.isUserIntercating) {
-      this.setState({
+    if (this.isUserIntercating) {
+      this.position = {
         x: e.clientX,
         y: e.clientY,
-      });
-      
+      };
     }
   };
   handlePointerUp = (e) => {
-    e.stopImmediatePropagation();
-    this.setState({ isUserIntercating: false }
-    );
-    this.domRef.current.removeEventListener(
-      "pointermove",
-      this.handlePointerMove,
-      false
-    );
+    this.isUserIntercating = false;
+    window.removeEventListener("pointermove", this.handlePointerMove, false);
     this.domRef.current.removeEventListener(
       "pointerup",
       this.handlePointerUp,
       false
     );
   };
-  handlePointerLeave = (e) => {
-    this.setState({ isUserIntercating: false });
-    e.stopImmediatePropagation();
-    this.setState({ isUserIntercating: false });
-    this.domRef.current.removeEventListener(
-      "pointermove",
-      this.handlePointerMove,
-      false
-    );
-    this.domRef.current.removeEventListener(
-      "pointerup",
-      this.handlePointerUp,
-      false
-    );
-  };
+
   componentDidMount() {
     this.domRef.current.addEventListener(
       "pointerdown",
       this.handlePointerDown,
       false
     );
-    this.domRef.current.addEventListener(
-      "pointerleave",
-      this.handlePointerLeave,
-      false
-    );
     this.props.scene.add(this.ball);
+    const update = () => {
+      requestAnimationFrame(update);
+      if (this.isUserIntercating) {
+        this.domRef.current.style.transform = `translate(-50%,-50%) translate(${this.position.x}px,${this.position.y}px)`;
+      } else {
+        const { posX, posY, visibility } = toScreenPosition(
+          this.ball,
+          this.props.camera,
+          this.props.renderer,
+          this.props.scene
+        );
+        this.position.x = posX;
+        this.position.y = posY;
+
+        this.domRef.current.style.transform = `translate(-50%,-50%) translate(${this.position.x}px,${this.position.y}px)`;
+        this.domRef.current.style.display = visibility;
+      }
+    };
+    update();
   }
   render() {
     return (
       <div
         className={styles.hotspot}
         style={{
-          transform: `translate(-50%,-50%) translate(${this.state.x}px,${this.state.y}px)`,
+          transform: `translate(-50%,-50%) translate(${this.position.x}px,${this.position.y}px)`,
         }}
         ref={this.domRef}
       >
--- src/pages/panorama-page/components/scene/Scene.component.jsx
+++ src/pages/panorama-page/components/scene/Scene.component.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Hotspot from '../hotpsot/hotspot.component'
+import Hotspot from "../hotpsot/hotspot.component";
 import styles from "./scene.module.scss";
 import * as THREE from "three";
 import { PerspectiveCamera, Raycaster, Vector2, Vector3 } from "three";
@@ -9,7 +9,7 @@ class Scene extends React.Component {
   constructor() {
     super();
     this.sceneRef = React.createRef();
-    this.renderer = new THREE.WebGLRenderer({antialias:true});
+    this.renderer = new THREE.WebGLRenderer({ antialias: true });
     this.renderer.setSize(window.innerWidth, window.innerHeight);
     this.scene = new THREE.Scene();
     this.camera = new PerspectiveCamera(
@@ -18,56 +18,48 @@ class Scene extends React.Component {
       0.1,
       1000
     );
-    
     this.camera.target = new THREE.Vector3(0, 0, 0);
-    
-    this.state = {
-      isUserInteracting: false,
-      onPointerDownMouseX: 0,
-      onPointerDownMouseY: 0,
-      lon: 0,
-      onPointerDownLon: 0,
-      lat: 0,
-      onPointerDownLat: 0,
-      phi: 0,
-      theta: 0,
-    };
+
+    this.isUserInteracting = false;
+    this.onPointerDownMouseX = 0;
+    this.onPointerDownMouseY = 0;
+    this.lon = 0;
+    this.onPointerDownLon = 0;
+    this.lat = 0;
+    this.onPointerDownLat = 0;
+    this.phi = 0;
+    this.theta = 0;
   }
 
   onPointerMove = (event) => {
     if (event.isPrimary === false) return;
-    this.setState({
-      lon:
-        (this.state.onPointerDownMouseX - event.clientX) * 0.1 +
-        this.state.onPointerDownLon,
-      lat:
-        (event.clientY - this.state.onPointerDownMouseY) * 0.1 +
-        this.state.onPointerDownLat,
-    });
-  };
+      this.lon = (this.onPointerDownMouseX - event.clientX) * 0.1 + this.onPointerDownLon
+      this.lat = (event.clientY - this.onPointerDownMouseY) * 0.1 + this.onPointerDownLat
+
+  }
 
   onPointerUp = (event) => {
     if (event.isPrimary === false) return;
-    this.setState({ isUserInteracting: false });
+    this.isUserInteracting = false
 
     this.sceneRef.current.removeEventListener(
       "pointermove",
       this.onPointerMove
     );
     this.sceneRef.current.removeEventListener("pointerup", this.onPointerUp);
-    this.sceneRef.current.removeEventListener("pointermove", this.onPointerMove);
+    this.sceneRef.current.removeEventListener(
+      "pointermove",
+      this.onPointerMove
+    );
   };
 
   onPointerDown = (event) => {
-    if (event.isPrimary === false ) return;
-    console.log(event.target.className )
-    this.setState({
-      isUserInteracting: true,
-      onPointerDownMouseX: event.clientX,
-      onPointerDownMouseY: event.clientY,
-      onPointerDownLon: this.state.lon,
-      onPointerDownLat: this.state.lat,
-    });
+    if (event.isPrimary === false) return;
+    this.isUserInteracting = true
+    this.onPointerDownMouseX = event.clientX
+    this.onPointerDownMouseY = event.clientY
+    this.onPointerDownLon = this.lon
+    this.onPointerDownLat = this.lat
     this.sceneRef.current.addEventListener(
       "pointermove",
       this.onPointerMove,
@@ -83,19 +75,16 @@ class Scene extends React.Component {
       this.onPointerDown,
       false
     );
-    
   };
 
   onWindowResize = () => {
-
     this.camera.aspect = window.innerWidth / window.innerHeight;
     this.camera.updateProjectionMatrix();
     this.renderer.setSize(window.innerWidth, window.innerHeight);
   };
-  
-  componentDidMount() {
 
-    window.onresize = this.onWindowResize
+  componentDidMount() {
+    window.onresize = this.onWindowResize;
     this.sceneRef.current.addEventListener(
       "pointerup",
       this.onPointerUp,
@@ -106,7 +95,7 @@ class Scene extends React.Component {
       this.onPointerDown,
       false
     );
-    
+
     const geometry = new THREE.SphereBufferGeometry(500, 80, 40);
     geometry.scale(-1, 1, 1);
     var texture = new THREE.TextureLoader().load(
@@ -116,30 +105,30 @@ class Scene extends React.Component {
       map: texture,
       wireframe: true,
     });
-    geometry.computeBoundingSphere()
+    geometry.computeBoundingSphere();
     const sphere = new THREE.Mesh(geometry, material);
     console.log(sphere);
     this.scene.add(sphere);
 
-    const temp_geometry = new THREE.SphereBufferGeometry(10, 10, 10)
+    const temp_geometry = new THREE.SphereBufferGeometry(10, 10, 10);
     const temp_material = new THREE.MeshBasicMaterial({
-      color: 0xFFFFFF,
+      color: 0xffffff,
       wireframe: true,
     });
-    const ball = new THREE.Mesh( temp_geometry, temp_material )
-    ball.position.set(10, 0, 100)
-    this.scene.add(ball)
+    const ball = new THREE.Mesh(temp_geometry, temp_material);
+    ball.position.set(10, 0, 100);
+    this.scene.add(ball);
     this.renderer.domElement.className = styles.canvas;
     this.sceneRef.current.appendChild(this.renderer.domElement);
-    
-    let ray = new THREE.Raycaster()
-    let mouse = new Vector2()
 
-    const onMouseMove = (e)=>{
-      mouse.x = (e.clientX/window.innerWidth) * 2 - 1;
-      mouse.y = - (e.clientY/window.innerHeight) * 2 + 1;
-    }
-    window.addEventListener('mousemove', onMouseMove, false)
+    let ray = new THREE.Raycaster();
+    let mouse = new Vector2();
+
+    const onMouseMove = (e) => {
+      mouse.x = (e.clientX / window.innerWidth) * 2 - 1;
+      mouse.y = -(e.clientY / window.innerHeight) * 2 + 1;
+    };
+    window.addEventListener("mousemove", onMouseMove, false);
     const animate = () => {
       requestAnimationFrame(animate);
       // ray.setFromCamera(mouse, this.camera)
@@ -149,35 +138,28 @@ class Scene extends React.Component {
       //   ball.position.copy(intersects[0].point)
       // }
 
-      if (this.state.isUserInteracting === false) {
-        this.setState((prevState) => {
-          return { lon: prevState.lon + 0.01 };
-        });
+      if (this.isUserInteracting === false) {
+        this.lon = this.lon + 0.01 
       }
-      this.setState((prevState) => {
-        return {
-          lat: Math.max(-85, Math.min(85, prevState.lat)),
-          phi: THREE.MathUtils.degToRad(90 - prevState.lat),
-          theta: THREE.MathUtils.degToRad(prevState.lon),
-        };
-      });
+      
+      this.lat= Math.max(-85, Math.min(85, this.lat))
+      this.phi= THREE.MathUtils.degToRad(90 - this.lat)
+      this.theta= THREE.MathUtils.degToRad(this.lon)
+        
       this.camera.target.x =
-        500 * Math.sin(this.state.phi) * Math.cos(this.state.theta);
-      this.camera.target.y = 500 * Math.cos(this.state.phi);
+        500 * Math.sin(this.phi) * Math.cos(this.theta);
+      this.camera.target.y = 500 * Math.cos(this.phi);
       this.camera.target.z =
-        500 * Math.sin(this.state.phi) * Math.sin(this.state.theta);
+        500 * Math.sin(this.phi) * Math.sin(this.theta);
       this.camera.lookAt(this.camera.target);
       this.renderer.render(this.scene, this.camera);
     };
     animate();
   }
   render() {
-    
     return (
       <div className={styles.scene} ref={this.sceneRef}>
-        <Hotspot scene={this.scene}>
-          
-        </Hotspot>
+        <Hotspot scene={this.scene} camera = {this.camera}  renderer = {this.renderer} ></Hotspot>
       </div>
     );
   }
